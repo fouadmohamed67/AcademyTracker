1bd42f7d04920a756c8bc8e699352da6e6a04c22
fouadmohamed67 <fouadmohamedfouad67@gmail.com>
2023-08-30 23:02:46 +0300

    fix cors police error at app.js

--- connections/database.js
+++ connections/database.js
@@ -1,12 +1,12 @@
 const mysql=require("mysql2/promise")
 let connection;
 try {
-        connection=mysql.createPool({
-            host:"sql.freedb.tech",
-            user:"freedb_fouad",
-            password:"QV4jPdKnHd3?qMQ",
-            database:"freedb_acadmyTracker"
-        })
+    connection=mysql.createPool({
+        host:"sql.freedb.tech",//sql.freedb.tech
+        user:"freedb_fouad",//freedb_fouad
+        password:"QV4jPdKnHd3?qMQ",//QV4jPdKnHd3?qMQ
+        database:"freedb_acadmyTracker"//freedb_acadmyTracker
+    })
         console.log('connected')
 } catch (error) {
     console.log('not connected')
