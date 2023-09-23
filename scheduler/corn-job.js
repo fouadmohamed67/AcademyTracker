a375a2771ec85f0e9cf6ca7fedd2ae5e999cce43
fouad mohamed <61196994+fouadmohamed67@users.noreply.github.com>
2023-09-23 03:42:24 +0300

    Update corn-job.js

--- scheduler/corn-job.js
+++ scheduler/corn-job.js
@@ -3,7 +3,7 @@ const lessonController=require('../controllers/lessonController');
 
 
 
-cron.schedule("* * * * *",async ()=>{
+cron.schedule("44 2 * * *",async ()=>{
     console.log(new Date().toUTCString());
     
    // await lessonController.generateDailyLessons();
